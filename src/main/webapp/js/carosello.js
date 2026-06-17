e7a97554aaa687519a051a6543f328a2a5be12cc
ThomasGiordano05 <t.giordano20@studenti.unisa.it>
2026-06-17 19:13:39 +0200

    Correzioni Index.jsp

--- src/main/webapp/js/carosello.js
+++ src/main/webapp/js/carosello.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll('.slide-img');
+    const dots = document.querySelectorAll('.dot');
     let index = 0;
     let autoScrollInterval;
 
     function showImage(i) {
         images.forEach(img => img.classList.remove('active'));
         images[i].classList.add('active');
+        updateDots(i);
+    }
+
+    function updateDots(i) {
+        dots.forEach(dot => dot.classList.remove('active-dot'));
+        dots[i].classList.add('active-dot');
     }
 
     function nextImage() {
@@ -27,6 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
         startAutoScroll();
     }
 
+    // Frecce
     document.querySelector('.right-changer').addEventListener('click', () => {
         nextImage();
         resetAutoScroll();
@@ -35,6 +43,15 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('.left-changer').addEventListener('click', () => {
         prevImage();
         resetAutoScroll();
+    });
+
+    // Click sui pallini
+    dots.forEach((dot, i) => {
+        dot.addEventListener('click', () => {
+            index = i;
+            showImage(index);
+            resetAutoScroll();
+        });
     });
 
     startAutoScroll();
